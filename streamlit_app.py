fc7baaac9439a1bca1f9edd22c8ed4a47d90ab03
Zulfiqar Ahmed <54955118+zabutt@users.noreply.github.com>
2024-01-31 14:23:41 +0000

    Update streamlit_app.py
    Fixed indention error in line chart.

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,7 +44,7 @@ def main():
 
     elif chart_type == "Line Chart":
         st.subheader("Line Chart")
-         fig = px.line(data, x='X', y='Y', line_shape='linear', line_dash=[color])
+        fig = px.line(data, x='X', y='Y', line_shape='linear', line_dash=[color])
         st.plotly_chart(fig)
 
   
